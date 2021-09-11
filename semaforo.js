fe33eff2b3e715058ab12259dad5c10a112e3635
Cauã Menezes <cauamenezes.07@gmail.com>
2021-09-11 13:57:14 -0300

    função de automatizar o farol finalizada e estilo dos botões modificado

--- semaforo.js
+++ semaforo.js
@@ -1,43 +1,42 @@
 "use strict";
 
-const 
-
 const getId = (elemento) => document.getElementById(elemento);
 
-const semaforo = getId("semaforo")
+let idVermelho;
+let idAmarelo;
+let idVerde;
+const semaforo = getId("semaforo");
 
 const tornarVermelho = () => {
-  semaforo.src = "img/vermelho.png"
+  semaforo.src = "img/vermelho.png";
 };
 
 const tornarAmarelo = () => {
-  semaforo.src = "img/amarelo.png"
+  semaforo.src = "img/amarelo.png";
 };
 
 const tornarVerde = () => {
-  semaforo.src = "img/verde.png"
+  semaforo.src = "img/verde.png";
 };
 
-const semaforoDesligado = () => semaforo.src.includes("desligado")
+const pararAutomatizar = () => {
+  clearInterval(idVermelho);
+  clearInterval(idAmarelo);
+  clearInterval(idVerde);
+};
 
-const automatico = () => {
-    const botaoAutomatico = getId("automatico")
-    
-}
-
-const automatizar = () => {
-    if(semaforo.src.includes("desligado")) {
-        semaforo.src = "img/vermelho"
-    } else if(semaforo.src.includes("vermelho")) {
-        semaforo.src = "img/amarelo.png"
-    } else if (semaforo.src.includes("amarelo")) {
-        semaforo.src = "img/verde.png"
-    } else if (semaforo.src.includes("verde")) {
-        semaforo.src = "img/vermelho.png"
-    } else {
-        semaforo.src = "img/vermelho.png"
-    }
-}
+const automatizarSemaforo = () => {
+  const botaoAutomatico = getId("automatico");
+  if (botaoAutomatico.textContent == "Automático") {
+    idVermelho = setInterval(tornarVermelho, 2300);
+    idAmarelo = setInterval(tornarAmarelo, 2400);
+    idVerde = setInterval(tornarVerde, 2500);
+    botaoAutomatico.textContent = "Parar";
+  } else {
+    pararAutomatizar();
+    botaoAutomatico.textContent = "Automático";
+  }
+};
 
 //eventos
 getId("vermelho").addEventListener("click", tornarVermelho);
@@ -46,4 +45,4 @@ getId("amarelo").addEventListener("click", tornarAmarelo);
 
 getId("verde").addEventListener("click", tornarVerde);
 
-getId("automatico").addEventListener("click", automatizar);
+getId("automatico").addEventListener("click", automatizarSemaforo);
